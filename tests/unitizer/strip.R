0fe5a474b5bc6d19008cc0dc8a2f7a27269fa734
brodieG <brodieG@users.noreply.github.com>
2017-10-16 18:34:19 +0200

    testup

--- tests/unitizer/strip.R
+++ tests/unitizer/strip.R
@@ -8,5 +8,18 @@ unitizer_sect("Strip ansi", {
   rgb.und <- "\033[4;38;2;0;120;200m"
 
   strip_ansi(sprintf("hello %sworld%s", red, end))
+  strip_ansi(sprintf("he%sllo %sworld", red, end))
+  strip_ansi(sprintf("%shello %sworld%s", grn.bg, red, end))
+  strip_ansi(sprintf("%s%shello %sworld%s", grn.bg, inv, red, end))
 
+  string <- paste("string", format(1:10))
+  string[c(2,4,6)] <- paste0(red, string[c(2,4,6)], end)
+
+  strip_ansi(string)
+})
+unitizer_sect("Corner cases", {
+  strip_ansi("hello\033")
+  # should this be stripped?  Not 100% clear since terminal seems to be waiting
+  # for input after it is cated
+  strip_ansi("hello\033[")
 })
