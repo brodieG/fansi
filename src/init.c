98e94d754ab4f2997645fa3ad3be64f2ad80243f
brodieG <brodieG@users.noreply.github.com>
2017-10-17 20:00:52 -0400

    adding init.c

---
+++ src/init.c
@@ -0,0 +1,21 @@
+
+#include "fansi.h"
+#include <R_ext/Rdynload.h>
+
+static const
+R_CallMethodDef callMethods[] = {
+  {"has_csi", (DL_FUNC) &FANSI_has, 1},
+  {NULL, NULL, 0}
+};
+
+void R_init_fansi(DllInfo *info)
+{
+ /* Register the .C and .Call routines.
+    No .Fortran() or .External() routines,
+    so pass those arrays as NULL.
+  */
+  R_registerRoutines(info, NULL, callMethods, NULL, NULL);
+  R_useDynamicSymbols(info, FALSE);
+  R_forceSymbols(info, FALSE);
+}
+
