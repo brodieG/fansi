ebc2b75f690b49f9520cc5cb9ba61035de6415ce
brodieG <brodieG@users.noreply.github.com>
2017-10-20 18:35:43 -0400

    only alloc string once for strip

--- src/strip.c
+++ src/strip.c
@@ -35,76 +35,110 @@ SEXP FANSI_strip(SEXP input) {
   SEXP res_fin = input;
 
   int any_ansi = 0;
+  int * which_ansi;            // track which elements contain ansi
+  R_len_t mem_req = 0;         // how much memory we need for each ansi
   R_xlen_t invalid_ansi = 0;
+
+  struct FANSI_csi_pos csi;
+
+  // First pass identify which items have ansi, and how many characters the
+  // longest of the lot has
 
   for(i = 0; i < len; ++i) {
     if(!i % 1000) R_CheckUserInterrupt();
-    const char * chr,  * chr_last,  * chr_track;
-    chr = chr_last = chr_track = CHAR(STRING_ELT(input, i));
-
-    char * res_track, * res_start;
-    int has_ansi = 0;
-    size_t i_len = strlen(chr);
-
-    if(i_len > INT_MAX)
-      // nocov start
-      error("Illegal size string encountered of size %.0f.", (double) i_len);
-      // nocov end
-
-    struct FANSI_csi_pos csi;
-
-    while((csi = FANSI_find_csi(chr_track)).start) {
-      if(csi.valid) {
-        if(!has_ansi) {
-          // Overallocate for simplicity, in reality shouldn't need to
-          // allocate for all the ansi tags we're stripping
-          res_start = res_track = (char *) R_alloc(i_len, sizeof(char));
-          res_start[i_len - 1] = 0;
-          has_ansi = any_ansi = 1;
-        }
-        // Is memcpy going to cause problems again by reading past end of
-        // block?  Didn't in first valgrind check.
-
-        if(csi.len) {
-          memcpy(res_track, chr_track, csi.start - chr_track);
-          res_track += csi.start - chr_track;
-        }
-      } else {
-        // Mark vector loc of first invalid ansi (should we warn here)?
-
-        if(!invalid_ansi) {
-          invalid_ansi = i;
-          warning("Invalid CSI len: %d", csi.len);
-        }
+    SEXP input_chr = STRING_ELT(input, i);
+    if(input_chr == NA_STRING) continue;
+    const char * chr = CHAR(input_chr);
+    csi = FANSI_find_csi(chr);
+    if(csi.valid) {
+      // As soon as we encounter ansi, allocate vector to track what has ansi
+      if(!any_ansi) {
+        any_ansi = 1;
+        which_ansi = (int *) R_alloc(len, sizeof(int));
+        for(R_len_t j = 0; j < len; ++j) which_ansi[j] = 0;
       }
-      chr_track = csi.start + csi.len;
-    }
-    // First time we encountere ANSI in our input vector we need to allocate the
-    // result vector
-
-    if(any_ansi && res_fin == input) {
-      REPROTECT(res_fin = duplicate(input), ipx);
-    }
-    if(has_ansi) {
-      // Copy final chunk if it exists because above we only memcpy when we
-      // encounter the tag
-
-      if(*chr_track) {
-        const char * chr_end = strchr(chr_track, '\0');
-        if(!chr_end) {
-          // nocov start
-          error("Internal Error: failed to find str end, contact maintainer.");
-          // nocov end
-        } else if(chr_end > chr_track) {
-          memcpy(res_track, chr_track, chr_end - chr_track);
-          res_track += chr_end - chr_track;
-      } }
-      *res_track = '\0';
-      SEXP chr_sexp = PROTECT(mkChar(res_start));
-      SET_STRING_ELT(res_fin, i, chr_sexp);
-      UNPROTECT(1);
+      which_ansi[i] = 1;
+      R_len_t chr_len = LENGTH(input_chr);
+      if(chr_len > mem_req) mem_req = chr_len;
     }
   }
+  // Now strip
+
+  if(any_ansi) {
+    // We need to allocate a result vector since we'll be stripping ANSI CSI
+
+    REPROTECT(res_fin = duplicate(input), ipx);
+
+    // Allocate memory sufficient for largest string.  Overallocate for
+    // simplicity, in reality shouldn't need to allocate for all the ansi tags
+    // we're stripping
+
+    if(mem_req == R_LEN_T_MAX)
+      // nocov start
+      error(
+        "%s%s",
+        "Internal error, string should be shorter than R_LEN_T_MAX, ",
+        "contact maintainer."
+      );
+      // nocov end
+
+    char * chr_buff = (char *) R_alloc(mem_req + 1, sizeof(char));
+
+    for(i = 0; i < len; ++i) {
+      if(!i % 1000) R_CheckUserInterrupt();
+      if(which_ansi[i]) {
+        const char * chr,  * chr_last,  * chr_track;
+        SEXP input_chr = STRING_ELT(input, i);
+        chr = chr_last = chr_track = CHAR(input_chr);
+
+        char * res_track, * res_start;
+
+        while((csi = FANSI_find_csi(chr_track)).start) {
+          if(csi.valid) {
+            res_start = res_track = chr_buff;
+
+            // Is memcpy going to cause problems again by reading past end of
+            // block?  Didn't in first valgrind check.
+
+            if(csi.len) {
+              memcpy(res_track, chr_track, csi.start - chr_track);
+              res_track += csi.start - chr_track;
+            }
+          } else {
+            // Mark vector loc of first invalid ansi (should we warn here)?
+
+            if(!invalid_ansi) {
+              invalid_ansi = i;
+              warning("Invalid CSI len: %d", csi.len);
+            }
+          }
+          chr_track = csi.start + csi.len;
+        }
+        // Copy final chunk if it exists because above we only memcpy when we
+        // encounter the tag
+
+        if(*chr_track) {
+          const char * chr_end = chr + LENGTH(input_chr) + 1;
+          if(!chr_end) {
+            // nocov start
+            error(
+              "%s%s",
+              "Internal Error: failed to find str end, ",
+              "contact maintainer."
+            );
+            // nocov end
+          } else if(chr_end > chr_track) {
+            memcpy(res_track, chr_track, chr_end - chr_track);
+            res_track += chr_end - chr_track;
+        } }
+        *res_track = '\0';
+        SEXP chr_sexp = PROTECT(
+          mkCharLenCE(
+            res_start, res_track - res_start, getCharCE(input_chr)
+        ) );
+        SET_STRING_ELT(res_fin, i, chr_sexp);
+        UNPROTECT(1);
+  } } }
   UNPROTECT(1);
   return res_fin;
 }
