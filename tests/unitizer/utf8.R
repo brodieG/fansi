4d785146bb3ebfb04407c0c76da403e08fb46d0d
brodieG <brodieG@users.noreply.github.com>
2017-10-28 13:48:32 -0400

    starting utf8 tests

---
+++ tests/unitizer/utf8.R
@@ -0,0 +1,25 @@
+# Test UTF-8, separate to avoid issues with platforms that don't support it
+
+library(fansi)
+
+unitizer_sect("substr", {
+  lorem.cn.pieces <-
+    substr(rep(lorem.cn, 5), c(1, 11, 21, 31), c(10, 15, 22, 45))
+
+  lorem.cn.col.1 <- paste0(
+    red, lorem.cn.pieces[1], inv, lorem.cn.pieces[2], grn.bg,
+    lorem.cn.pieces[3], rgb.und, lorem.cn.pieces[4], end
+  )
+  starts <- seq(1, 17, 4)
+  ends <- starts + 3
+  ansi_substr2(rep(lorem.cn.col.1, 5), starts, ends)
+
+  lorem.cn.col.2 <- paste0(
+    red, lorem.cn.pieces[1], "hello", inv, lorem.cn.pieces[2], " there ",
+    grn.bg, lorem.cn.pieces[3], rgb.und, lorem.cn.pieces[4], end
+  )
+  starts <- seq(1, by=6, len=5)
+  ends <- starts + 5
+  ansi_substr2(rep(lorem.cn.col.2, 5), starts, ends)
+
+})
