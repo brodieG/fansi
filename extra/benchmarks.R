5434e524c6a319af3e46928fd2df2f90f17a16b5
brodieG <brodieG@users.noreply.github.com>
2018-02-12 21:18:58 -0500

    benchmarking as html

--- extra/benchmarks.R
+++ extra/benchmarks.R
@@ -1,5 +1,7 @@
 library(fansi)
-ulysses <- readLines("http://www.gutenberg.org/files/4300/4300-0.txt")
+ulysses <- readLines(
+  "http://www.gutenberg.org/files/4300/4300-0.txt", encoding='UTF-8'
+)
 ulysses.c <- paste0(ulysses, collapse='\n')
 n <- 1e4
 starts <- 1:n
@@ -23,16 +25,35 @@ ulysses.clr[close] <- paste0(ulysses.clr[close], '\033[m')
 
 system.time(csi.clr <- fansi::strwrap_esc(ulysses.clr, 30))
 
-r.thanks <- ulysses
-bg <- ceiling((seq_along(r.thanks)) %% 215 + 1) + 16
+ulysses <- readLines(
+  "http://www.gutenberg.org/files/4300/4300-0.txt", encoding='UTF-8'
+)
+bg <- ceiling((seq_along(ulysses)) %% 215 + 1) + 16
 fg <- ifelse((((bg - 16) %/% 18) %% 2), 30, 37)
 tpl <- "\033[%d;48;5;%dm%s\033[49m"
 
 ## Apply colors to strings and collapse
+
 nz <- nzchar(r.thanks)
 r.thanks[nz] <- sprintf(tpl, fg[nz], bg[nz], r.thanks[nz])
 r.col <- paste0(r.thanks, collapse="\n")
 
 ## Wrap and display
-r.wrap <- strwrap2_esc(r.col, 25, pad.end=" ", wrap.always=TRUE)
+system.time(r.wrap <- strwrap2_esc(r.col, 25, pad.end=" ", wrap.always=TRUE))
+cols <- c("", paste(" ", r.wrap[1:27], " ", r.wrap[28:54]), "")
+
+cols.html <- esc_to_html(cols)
+tmp <- tempfile()
+writeLines(esc_to_html(cols), tmp)
+writeLines(
+  c("<html><meta charset='UTF-8'><pre>", cols.html, "</pre></html>"), tmp
+)
+browseURL(tmp)
+
+
+
+
 writeLines(c("", paste(" ", r.wrap[1:27], " ", r.wrap[28:54]), ""))
+
+#
+#r.wrap <- strwrap2_esc(substr(r.col,1,200), 25, pad.end=" ", wrap.always=TRUE)
