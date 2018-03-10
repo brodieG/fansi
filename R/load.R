444e7fe781c005fb35d5c6cf9d38545ba17444c9
brodieG <brodieG@users.noreply.github.com>
2018-03-10 16:17:08 -0500

    fix . in globals

--- R/load.R
+++ R/load.R
@@ -47,6 +47,6 @@
 # Need global declaration so that `vetr` doesn't issue R CMD build warnings due
 # to the use of `.`
 
-if(getRversion() >= "2.15.1")  utils::globalVariables(c("a", "othervar"))
+if(getRversion() >= "2.15.1")  utils::globalVariables(".")
 
 # nocov end
