7a566b92e1ce8519346189bdd768e7f47ca71065
brodieG <brodieG@users.noreply.github.com>
2021-10-28 19:53:58 -0400

    'has' only warns on malformed

--- src/has.c
+++ src/has.c
@@ -32,11 +32,12 @@ SEXP FANSI_has(SEXP x, SEXP ctl, SEXP warn) {
   SEXP res = PROTECT(allocVector(LGLSXP, len));
   int * res_int = LOGICAL(res);
   struct FANSI_state state;
-  unsigned int warn_int = asLogical(warn) * FANSI_WARN_ALL;
+  unsigned int warn_int = asLogical(warn) * FANSI_WARN_CSIBAD;
 
   for(R_xlen_t i = 0; i < len; ++i) {
     if(!i) {
       state = FANSI_state_init_ctl(x, warn, ctl, i);
+      state.warn = warn_int;
     } else {
       state = FANSI_state_reinit(state, x, i);
       state.warn = warn_int;
