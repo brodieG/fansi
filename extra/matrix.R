f4a5e0596409afc45fa1988de2b12536add290ad
brodieG <brodieG@users.noreply.github.com>
2022-01-07 07:57:14 -0500

    complete multi-clip support

--- extra/matrix.R
+++ extra/matrix.R
@@ -76,16 +76,27 @@ fansi.end <- 125
 fansi.ramp <- 50
 dim.start <- 25
 
+add_noise <- function(len, base, noise) {
+  round(pmax(0, pmin(base + (runif(len) - .5) * noise, 255)))
+}
+
 # @param frames total frames
 # @param start frame to start displaying text
 # @param end frame to end displaying text
 # @param ramp how many frames to dedicate to the text brightness ramp
 # @param fade how many frames to fade to black with
+# @param active a list of character states
 
-make_frames <- function(
-  idx, text, frames, start, end, ramp, fade, active=list()
-) {
-  stopifnot(ramp >= 0, fade >= 0)
+make_frames <- function(idx, text, frames, fade, logo, active=list()) {
+  stopifnot(fade >= 0, frames >= logo$end)
+  logo.ramp <- with(
+    logo,
+    approx(
+      c(1, start, start + fade.in[1], end - fade.out[1],         end, frames),
+      c(0,     0,         fade.in[2],        fade.in[2], fade.out[2], fade.out[2]),
+      seq_len(frames)
+  ) )
+
   res <- character(frames)
   for(f in seq_len(frames)) {
     dim <- if(fade) min(c(1, (frames - f) / fade)) else 1
@@ -96,6 +107,9 @@ make_frames <- function(
       },
       active
     )
+    # In each frame, 1/8th of the columns are picked for highlighting, and a
+    # sequence random length of decreasing brightnesses is generated for them,
+    # These are added to previously frame's highlight data.
     active <- c(
       active,
       lapply(
@@ -108,6 +122,7 @@ make_frames <- function(
               pos=seq(1, length.out=len, by=-1),
               val=(len - seq_len(len) + 1) / len
     ) ) } ) )
+    # Drop any positions that are off-screen
     active.in <- lapply(
       active,
       function(x) {
@@ -115,6 +130,7 @@ make_frames <- function(
         x[['dat']] <- x[['dat']][pos > 0 & pos <= nrow, ,drop=FALSE]
         x
     } )
+    # Map brightness to grid, newer instances overwrite older ones
     bright <- matrix(0, ncol, nrow)
     for(i in active.in) {
       bright[i[['row']], i[['dat']][,'pos']] <- i[['dat']][,'val']
@@ -131,24 +147,24 @@ make_frames <- function(
       display[is.bright]
     )
     display[!is.bright] <- "  "
-    if(f >= start) {
-      if(ramp >= 1) {
-        f.bright.base <- min(
-          c(f - start) / ramp, c(end - f) / ramp, 1
-        )
-      } else f.bright.base <- 1
+    # Start display of inside logo by changing background color, subject to ramp
+    # rates for fade-in/out
 
-      f.bright <- f.bright.base * (1 - runif(nrow(idx)) * .2)
-      display[idx] <- sprintf(
-        "\033[48;2;0;%d;0m%s\033[m",
-        round(f.bright * 180 * dim), display[idx]
-      )
-    }
+    logo.bright <- logo.ramp$y[f]
+    noise <- if(f >= logo$noise[1]) logo$noise[2] else 0
+    display[idx] <- sprintf(
+      "\033[48;2;%d;%d;%dm%s\033[m",
+      round(add_noise(nrow(idx), logo.bright, noise) * logo$rgb[1] * dim),
+      round(add_noise(nrow(idx), logo.bright, noise) * logo$rgb[2] * dim),
+      round(add_noise(nrow(idx), logo.bright, noise) * logo$rgb[3] * dim),
+      display[idx]
+    )
     res[f] <- paste0(
       paste0(rbind(display, '\n'), collapse=''),
       sprintf('\033[%dA\r', nrow + 1),
       collapse=""
     )
+    # Shift all the highlight info down one row
     active <- lapply(active,
       function(x) {
         x[['dat']][,'pos'] <- x[['dat']][,'pos'] + 1
@@ -167,14 +183,54 @@ take <- function(text) {
   }
   writeLines(character(nrow))
 }
+# @param start start frame for logo
+# @param end end frame logo
+# @param fade.in numeric(2) how many frames to fade in over, and brightness to
+#   fade in to.
+# @param fade.out numeric(2) how many frames to fade out over, and brightness to
+#   fade out to.
+# @param noise numeric(2) what frame to start noise, and the magnitude of it
+# @param rgb numeric(3) channel weighting
+
+logo_dat <- function(start, end, fade.in, fade.out, noise, rgb=c(0,1,0)) {
+  start
+  noise
+  stopifnot(
+    is.numeric(c(start, end, fade.in, fade.out, noise, rgb)),
+    start >= 1, end > start,
+    fade.in[1] + fade.out[1] < end - start,
+    fade.in[2] >= 0 && fade.in[2] <= 255,
+    fade.out[2] >= 0 && fade.out[2] <= 255,
+    noise[1] >= start,
+    noise[1] <= end,
+    noise[2] >= 0 && noise[2] <= 255,
+    rgb >= 0 & rgb <= 1
+  )
+  list(
+    start=start, end=end, fade.in=fade.in, fade.out=fade.out, noise=noise,
+    rgb=rgb
+  )
+}
 stop('ready to go')
-# fansi <- make_frames(raw_to_mx(fansi.raw), 150, 40, 125, 50, 25)
+
+text <- matrix(sample(char.pool, ncol * nrow, replace=TRUE), ncol)
 
 fansi.idx <- raw_to_mx(fansi.raw, nrow, ncol)
-text <- matrix(sample(char.pool, ncol * nrow, replace=TRUE), ncol)
-fansi <- make_frames(fansi.idx, text, 100, 20, 80, 20, 0)
+fansi.logo <- logo_dat(
+  start=25, end=65, fade.in=c(15, 255 * .7), fade.out=c(20, 0),
+  noise=c(start + 10, 20)
+)
+fansi <- make_frames(fansi.idx, text, frames=75, fade=0, logo=fansi.logo)
 
 one.oh.idx <- raw_to_mx(one.oh.raw, nrow, ncol)
-oneoh <- make_frames(one.oh.idx, fansi$text, 50, 5, 45, 1, 1, active=fansi$active)
-
+one.oh.logo <- logo_dat(
+  start=1, end=50, fade.in=c(1, 255), fade.out=c(0, 255),  # no fade out
+  noise=c(10, 250), rgb=c(1,1,1)
+)
+oneoh <- make_frames(
+  one.oh.idx, fansi$text, frames=50, fade=20, logo=one.oh.logo,
+  active=fansi$active
+)
+# take(c(oneoh$frames))
+# take(c(fansi$frames))
 take(c(fansi$frames, oneoh$frames))
