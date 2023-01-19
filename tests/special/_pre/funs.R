af2c46044cd45da9aaedab9397f50120b88790c1
brodieG <brodieG@users.noreply.github.com>
2023-01-18 20:45:21 -0500

    switch to curl style copyright

--- tests/special/_pre/funs.R
+++ tests/special/_pre/funs.R
@@ -1,3 +1,18 @@
+## Copyright (C) Brodie Gaslam
+##
+## This file is part of "fansi - ANSI Control Sequence Aware String Functions"
+##
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 2 or 3 of the License.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## Go to <https://www.r-project.org/Licenses> for copies of the licenses.
+
 ## Helpers to extract the condition message only due to instability in
 ## C level error/warning in displaying the call or not.
 ##
