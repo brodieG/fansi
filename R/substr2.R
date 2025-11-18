8aec229564395d509bcd9d0b37d4c4d444962ef4
brodieG <brodieG@users.noreply.github.com>
2025-11-18 18:25:15 -0500

    update docs

--- R/substr2.R
+++ R/substr2.R
@@ -95,10 +95,10 @@
 #' **In general it is best not to rely on exact `fansi` output, e.g. by
 #' embedding it in tests**.
 #'
-#' Width and grapheme calculations depend on locale, Unicode database
-#' version (see [`fansi_unicode_version`], and grapheme processing logic,
-#' among other things (see "Graphemes").  Individual character width are
-#' intended to match R4.5.1 definitions, except for differences introduced by
+#' Width and grapheme calculations depend on Unicode database version (see
+#' [`fansi_unicode_version`], and grapheme processing logic among other
+#' things (see "Graphemes").  Individual character width are intended to match
+#' R4.5.1 definitions in an English locale, except for differences introduced by
 #' Unicode Database Version updates and grapheme processing.
 #'
 #' How a particular display format is encoded in _Control Sequences_ is
